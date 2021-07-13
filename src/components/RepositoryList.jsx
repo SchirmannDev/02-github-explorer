3e8c4906b835d28bcb1b3c321702a1c498abdfa2
Cintia Schirmann <cintiaschirmann@Cintias-MacBook-Pro.local>
2021-07-13 16:09:19 -0300

    componente repository criado

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,26 @@
 export function RepositoryList() {
   return (
 <section className="repository-list">
+  <h1>Lista de Repositorios</h1>
+  <ul>
+    <li>
+      <strong>unform</strong>
+      <p>Formns in React</p>
+      <a href="">Acessar repositorio</a>
+    </li>
+
+    <li>
+      <strong>unform</strong>
+      <p>Formns in React</p>
+      <a href="">Acessar repositorio</a>
+    </li>
+
+    <li>
+      <strong>unform</strong>
+      <p>Formns in React</p>
+      <a href="">Acessar repositorio</a>
+    </li>
+  </ul>
 
 </section>
   );
