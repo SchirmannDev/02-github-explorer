37a6b70c33aaf6ffcd70166d738db3cd33163742
Cintia Schirmann <cintiaschirmann@Cintias-MacBook-Pro.local>
2021-07-12 23:04:37 -0300

    modificiando index

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 export function App(){
-  return <h1>Hello World Bitches</h1>
+  return <h1>test</h1>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
+import React from 'react';
 import {render } from "react-dom";
 import {App} from './App';
 
-render(<h1>Test</h1>, documente.getElementeById('root'))
+render(<h1>hello world</h1>, document.getElementById('root'))
